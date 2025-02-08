6fbd1c7cbe5fb4368e50671e7e221c757c30d731
Foresteh <luckysandon3@gmail.com>
2025-02-08 19:06:22 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-document.getElementById("showPopup").addEventListener("click", function() {
+function openModal() {
     document.getElementById("popup").style.display = "flex";
-});
+}
 
-document.getElementById("closePopup").addEventListener("click", function() {
+function closeModal() {
     document.getElementById("popup").style.display = "none";
-});
+}
+
+// Add event listeners for both click and touch events
+document.getElementById("showPopup").addEventListener("click", openModal);
+document.getElementById("showPopup").addEventListener("touchstart", openModal);
+
+document.getElementById("closePopup").addEventListener("click", closeModal);
+document.getElementById("closePopup").addEventListener("touchstart", closeModal);
