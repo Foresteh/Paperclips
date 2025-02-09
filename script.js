f7aeff4f0fe764027d482786050f991b883a274b
Foresteh <luckysandon3@gmail.com>
2025-02-09 22:56:16 +1000

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ function closeModal() {
     // sets the inline style display as "none", which will hide the element
 }
 
-// Add event listeners for both click and touch events
+// Add event listeners for both click and touch events to support pc and mobile use
 document.getElementById("showPopup").addEventListener("click", openModal);
+// .addEventListener activates the function "openModal" when the element "showPopup" is clicked
 document.getElementById("showPopup").addEventListener("touchstart", openModal);
+// touchstart is when the user begins the touch unlinke touchend or touchmove.
 
 document.getElementById("closePopup").addEventListener("click", closeModal);
 document.getElementById("closePopup").addEventListener("touchstart", closeModal);
