c6bb1904ce69162bfd02ebaffb8be8b28de5c13f
Foresteh <luckysandon3@gmail.com>
2025-02-11 00:59:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let paperclip = 0
+const notValelements = document.querySelectorAll('.notValentines');
+const Valelements = document.querySelectorAll('.Valentines');
+const upgradeelements = document.querySelectorAll('.upgrades');
+
+function clickButton() {
+    paperclip++; // Increment the count by 1
+    if (paperclip == 1) {
+        document.getElementById('countPaperclips').style.display = "flex";
+    }
+    document.getElementById('countPaperclips').innerText = `You have ${paperclip} paperclips`;
+    if (paperclip == 3) {
+        upgradeelements.forEach(element => {element.style.display = "flex";});
+    }
+}
+
 function openModal() {
     // defines function with name "openModal"
     document.getElementById("popup").style.display = "flex";
@@ -5,17 +21,22 @@ function openModal() {
     // sets the inline style display as "flex", which will auto centre and display the element
 }
 
-function closeModal() {
+function declinePopup() {
     // defines function with name "closeModal"
     document.getElementById("popup").style.display = "none";
     // sets the inline style display as "none", which will hide the element
 }
 
+function acceptPopup() {
+    notValelements.forEach(element => {element.style.display = "none";});
+    Valelements.forEach(element => {element.style.display = "flex";});
+}
+
 // Add event listeners for both click and touch events to support pc and mobile use
-document.getElementById("showPopup").addEventListener("click", openModal);
+document.getElementById("showPopup").addEventListener("click", clickButton);
 // .addEventListener activates the function "openModal" when the element "showPopup" is clicked
-document.getElementById("showPopup").addEventListener("touchstart", openModal);
+document.getElementById("showPopup").addEventListener("touchstart", clickButton);
 // touchstart is when the user begins the touch unlike touchend or touchmove.
 
-document.getElementById("closePopup").addEventListener("click", closeModal);
-document.getElementById("closePopup").addEventListener("touchstart", closeModal);
+document.getElementById("closePopup").addEventListener("click", declinePopup);
+document.getElementById("closePopup").addEventListener("touchstart", declinePopup);
